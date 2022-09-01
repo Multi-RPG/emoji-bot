a008103b608df1238259124fe6805d4b5addf09f
W Gomes <w.gomes5521250@gmail.com>
2022-09-01 03:36:04 -0300

    added paginator to command /emojis

---
+++ emojibot/pages.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+import logging
+import discord
+
+from typing import List, Tuple
+
+from discord.ext.pages import PaginatorButton
+
+log = logging.getLogger("emobot")
+
+class Common_Buttons:
+    def __init__(self):
+        self.page_buttons: List[PaginatorButton] = [
+            PaginatorButton(
+                "first",
+                label="<<-",
+                style=discord.ButtonStyle.green
+            ),
+            PaginatorButton(
+                "prev",
+                label="<-",
+                style=discord.ButtonStyle.green
+            ),
+            PaginatorButton(
+                "page_indicator",
+                style=discord.ButtonStyle.gray,
+                disabled=True
+            ),
+            PaginatorButton(
+                "next",
+                label="->",
+                style=discord.ButtonStyle.green
+            ),
+            PaginatorButton(
+                "last",
+                label="->>",
+                style=discord.ButtonStyle.green
+            ),
+        ]
+
+
+class Emoji_Pages(Common_Buttons):
+    def __init__(self, pages: List[discord.Embed]):
+        Common_Buttons.__init__(self)
+        self.pages: List[discod.Embed] = pages
+
+
+class Guild_Pages(Common_Buttons):
+    pass
